42b9abc861e875cfb72cfac416e4e16b0dc69a1f
Hugo Peres <hugo-pdf@hotmail.com>
2017-11-23 12:50:13 -0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,8 @@ $(document).ready(function (e) {
 
 
 function loadCarousel() {
-    request('/data/carrossel.json', function (data) {
+    // request('/data/carrossel.json', function (data) {
+    request('https://evoque-pizzaria.firebaseio.com/teste.json', function (data) {
         var slideList = [];
         var indicators = [];
         var carousels = JSON.parse(data);
